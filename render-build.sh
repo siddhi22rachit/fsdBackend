d25eff74576fcc63b5d4bbdfa37872a0676166d4
siddhi22rachit <siddhibswant2212@gmail.com>
2025-04-17 09:56:26 +0530

    add the render file

---
+++ render-build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Install Maven
+curl -fsSL https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
+apt-get update && apt-get install -y maven
+
+# Now build the project
+mvn clean package
